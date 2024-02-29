96c78ea19415dfa82caa265954803f2cabce79ca
parthsolanke <parthsolanke2002@gmail.com>
2024-02-29 07:47:30 +0530

    yolov9 detects person

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ model.conf = 0.25  # NMS confidence threshold
 model.iou = 0.45  # NMS IoU threshold
 model.classes = 0  # 0: class id for person
 
-video_path = "./data/input.mp4"
+video_path = "./data/inp.mp4"
 cap = cv2.VideoCapture(video_path)
 if not cap.isOpened():
     print("Error opening video file")
@@ -31,37 +31,12 @@ while cap.isOpened():
 
     # parse results
     predictions = results.pred[0]
-    boxes = predictions[:, :4]  # x1, y1, x2, y2
-
-    # add missing variables
-    scores = predictions[:, 4]  # scores
-    categories = predictions[:, 5]  # categories
+    boxes = predictions[:, :4]  # x1, y1, x2, y2s
 
     # draw bounding boxes on frame
-    for box, score, category in zip(boxes, scores, categories):
+    for box in boxes:
         x1, y1, x2, y2 = np.array(box, dtype=int)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 165, 255), 1, cv2.LINE_AA)
-        cv2.putText(
-            frame,
-            f"{score:.2f}",
-            (x1, y1 - 5),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.4,
-            (0, 165, 255),
-            1,
-            cv2.LINE_AA
-        )
-        category_name = model.names[int(category)]
-        cv2.putText(
-            frame,
-            category_name,
-            (x1, y1 - 20),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            (0, 165, 255),
-            1,
-            cv2.LINE_AA
-        )
           
     # display frame
     cv2.imshow("Frame", frame)
